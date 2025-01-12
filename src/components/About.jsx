c93ee3ad040caa4a9d2051f1cc4513205ac1407b
SehajpSingh <sehaj0918@gmail.com>
2025-01-11 18:12:50 -0800

    computer model and animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div> About </div>
+  )
+}
+export default About 
+
+/*import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { style } from 'framer-motion/client';
+import { strToU8 } from 'three/examples/jsm/libs/fflate.module.js';
+
+const About = () => {
+  return (
+  <>
+    <motion.div>
+      <p className={styles.sectionSubText}
+      >Introduction</p>
+      <h2 className={styles.sectionHeadText}
+      >Overview.</h2>
+    </motion.div>
+  </>
   )
 }
 
 export default About
+
+*/
