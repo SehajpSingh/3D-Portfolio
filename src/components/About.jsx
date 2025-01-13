d03d25ac7646b7dc44154ecccf8d87417d106678
SehajpSingh <sehaj0918@gmail.com>
2025-01-12 18:39:50 -0800

    working paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,15 +21,37 @@ const textVariant = () => ({
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 }
-})
+});
+
+const simpleVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 }
+};
 
 const About = () => {
   return (
+    <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-  )
+
+      <motion.p variants={simpleVariants}
+      className="mt-4 text-secondary text-[17px]
+      max-w-3xl leading-[30px">
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+          I'm a skilled worker.
+      </motion.p>
+      
+
+      </>
+  );
 }
 
 export default About;
