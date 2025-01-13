8dc79fd35d0077a60d2c4c9acea4f672c3200ec6
SehajpSingh <sehaj0918@gmail.com>
2025-01-13 15:32:21 -0800

    final working version of website with contact form and details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,39 @@
-/*import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-const About = () => {
-  return (
-    <div> About </div>
-  )
-}
-export default About 
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-*/
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
 
-
-import React from 'react'
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-
-const textVariant = () => ({
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 }
-});
-
-const simpleVariants = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 }
-};
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -36,24 +43,26 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p variants={simpleVariants}
-      className="mt-4 text-secondary text-[17px]
-      max-w-3xl leading-[30px">
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
-          I'm a skilled worker.
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Innovative Computer Science graduate from the University of New Mexico, 
+        with a strong foundation in full-stack development utilizing Java, Python, 
+        and React. Experienced in designing scalable web applications and optimizing 
+        databases, I have a proven track record of enhancing system performance and 
+        user experience. Passionate about leveraging technology to solve real-world 
+        problems, I am eager to contribute my skills and enthusiasm to dynamic and 
+        forward-thinking teams.
       </motion.p>
-      
 
-      </>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   );
-}
+};
 
-export default About;
-
-
+export default SectionWrapper(About, "about");
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,265 +1,275 @@
 import {
-    mobile,
-    backend,
-    creator,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "Web Developer",
-      icon: web,
-    },
-    {
-      title: "React Native Developer",
-      icon: mobile,
-    },
-    {
-      title: "Backend Developer",
-      icon: backend,
-    },
-    {
-      title: "Content Creator",
-      icon: creator,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-  ];
-  
-  const experiences = [
-    {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-  ];
-  
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: jobit,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Trip Guide",
-      description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-      tags: [
-        {
-          name: "nextjs",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
-    },
-  ];
-  
-  export { services, technologies, experiences, testimonials, projects };
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  tripguide,
+  threejs,
+} from "../assets";
+
+import jobit from '../assets/jobit.jpg';
+import ra from '../assets/RealityAI.jpg';
+import snl from '../assets/snl.png';
+import unmh from '../assets/hospital.png';
+import spring from '../assets/spring.png';
+import java from '../assets/java.png';
+import js from '../assets/js.svg';
+import unity from '../assets/unity.png';
+import ml from '../assets/ml.jpg';
+
+//import { spring } from '../../assets';
+
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Spring-Boot Developer",
+    icon: spring,
+  },
+  {
+    title: "Java",
+    icon: java,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "JavaScript",
+    icon: js,
+  },
+  {
+    title: "Unity",
+    icon: unity,
+  },
+  {
+    title: "Machine Learning",
+    icon: ml,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Developer",
+    company_name: "Sandia National Labs",
+    icon: snl,
+    iconBg: "#383E56",
+    date: "May 2022 - May 2024",
+    points: [
+      "Developed and maintained robust backend services using Spring Boot, enhancing application performance and scalability.",
+      "Applied advanced Java programming techniques to implement complex business logic and improve data processing capabilities.",
+      "Designed and integrated RESTful APIs, facilitating seamless communication between the frontend and backend components.",
+      "Managed and optimized SQL databases, ensuring efficient data storage, retrieval, and integrity across various application functions.",
+    ],
+  },
+  {
+    title: "Software Developer Intern",
+    company_name: "Sandia National Labs",
+    icon: snl,
+    iconBg: "#E6DEDD",
+    date: "May 2021 - May 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Software Intern",
+    company_name: "UNM Hospital",
+    icon: unmh,
+    iconBg: "#E6DEDD",
+    date: "Jan 2020 - Apr 2022",
+    points: [
+      "Enhanced search functionality by integrating metadata into legacy workflows, significantly improving data retrieval efficiency.",
+      "Optimized database performance using Apache Cassandra, enhancing data handling and storage operations.",
+      "Contributed to the development of healthcare communication tools to streamline patient-provider interactions.",
+      "Collaborated with a cross-functional team to implement scalable software solutions, ensuring robust application performance.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Sehaj excels in project management, consistently leading teams to deliver complex software projects on time and above expectations.",
+    name: "Allena Satapathi",
+    designation: "Division Manager",
+    company: "Sandia National Labs",
+    image: snl,
+  },
+  {
+    testimonial:
+      "A tech virtuoso, Sehaj skillfully merges cutting-edge front-end and back-end technologies to craft superior web applications.",
+    name: "Jessie Maxwell",
+    designation: "Tech Lead",
+    company: "UNM Hospital",
+    image: unmh,
+  },
+  {
+    testimonial:
+      "Sehaj's relentless pursuit of excellence and dedication to professional growth sets a high standard in every project he tackles.",
+    name: "Wilfredo Ramos",
+    designation: "Manager",
+    company: "Reality AI Lab",
+    image: ra,
+  },
+];
+
+const projects = [
+  {
+    name: "Job Portal",
+    description:
+      "A Full-Stack Spring Boot application integrated with an SQL database, designed to streamline the recruitment process. This platform enables recruiters to post job openings and allows candidates to efficiently search for and apply to jobs. The system is built to facilitate smoother interactions between job seekers and employers, leveraging advanced search functionalities and user-friendly interfaces for optimal usability.",
+    tags: [
+      {
+        name: "Java",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "SpringBoot",
+        color: "green-text-gradient",
+      },
+      {
+        name: "SQL",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/SehajpSingh/JobPortal",
+  },
+  {
+    name: "Scrabble",
+    description:
+      "This Scrabble project is a Java-based application hosted on GitHub that implements a classic word game with a graphical user interface using JavaFX. It features a sophisticated algorithm to determine valid word placements and calculate scores, enhancing the traditional gameplay experience. The project also incorporates a custom AI opponent, challenging players with strategic word placements. Designed to sharpen linguistic skills and provide entertainment, this application showcases advanced programming techniques and object-oriented design principles.",
+    tags: [
+      {
+        name: "Java",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JavaFX",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Data Structures",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/SehajpSingh/Scrabble",
+  },
+
+  /*
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  }, 
+  */
+];
+
+export { services, technologies, experiences, testimonials, projects };
